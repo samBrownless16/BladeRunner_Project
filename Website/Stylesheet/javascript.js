18bfb581e40b4d93b695c800cacaa7e0f62d3228
DAVID ALLEN <G00375372@gmit.ie>
2020-03-04 14:03:55 +0000

    Email sign up

---
+++ Website/Stylesheet/javascript.js
@@ -0,0 +1,9 @@
+//	=======================================================================================	
+//    - function peforms alert to acknowledge email sign up
+	function signUp()
+	{
+		alert("Your email was successfully added");// alert
+	}// END of signUp function
+	
+	
+//	=======================================================================================	
