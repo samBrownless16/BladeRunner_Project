227d04003928f844da7d90ae3ba9cbac811d5566
DAVID ALLEN <G00375372@gmit.ie>
2020-03-04 17:34:00 +0000

    events partial update

--- Website/Stylesheet/javascript.js
+++ Website/Stylesheet/javascript.js
@@ -2,7 +2,7 @@
 //    - function peforms alert to acknowledge email sign up
 	function signUp()
 	{
-		alert("Your email was successfully added");// alert
+		alert("Your email was successfully added to our mailing list");// alert
 	}// END of signUp function
 	
 	
