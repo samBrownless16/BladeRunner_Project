fe98aab07727ea456448c2c5c1d1186fef8e894c
DAVID ALLEN <G00375372@gmit.ie>
2020-03-11 22:16:05 +0000

    code validation

--- Website/Stylesheet/javascript.js
+++ Website/Stylesheet/javascript.js
@@ -5,5 +5,9 @@
 		alert("Your email was successfully added to our mailing list");// alert
 	}// END of signUp function
 	
-	
 //	=======================================================================================	
+//    - function peforms alert to acknowledge booking date at restaurant
+	function bookDate()
+	{
+		alert("Your booking has been confirmed");// alert
+	}// END of bookDate function
